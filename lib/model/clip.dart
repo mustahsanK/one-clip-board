32cc94d3495c934c2e0aa1824a97e700d8f20879
mustahsanK <mustahsan60@outlook.com>
2023-01-07 23:51:07 +0530

    added date field

--- lib/model/clip.dart
+++ lib/model/clip.dart
@@ -2,6 +2,7 @@ class Clip {
   final int uid;
   final String data;
   final String source;
+  final String date;
 
-  const Clip(this.uid, this.data, this.source);
+  const Clip(this.uid, this.data, this.source, this.date);
 }
