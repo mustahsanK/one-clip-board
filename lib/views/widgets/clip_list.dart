62564be535b9343765f10e2d691026607b821008
mustahsanK <mustahsan60@outlook.com>
2023-01-23 01:53:02 +0530

    Configured Clip List

---
+++ lib/views/widgets/clip_list.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:one_clip_board/model/clip.dart';
+import 'package:one_clip_board/services/sample_data/sample.dart';
+import 'package:one_clip_board/views/widgets/clip_list_item.dart';
+
+class ClipList extends StatelessWidget {
+  const ClipList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List<Clip> sampleClips = const Sample().getSample();
+    return ListView(
+      children: [
+        for (Clip clip in sampleClips)
+          ClipListItem(clip)
+      ],
+    );
+  }
+}
