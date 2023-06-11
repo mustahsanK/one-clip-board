e601833e6c8beeb13d4e4426fe07a874394c840a
mustahsanK <mustahsan60@outlook.com>
2023-06-11 23:39:22 +0530

    hide status bar and debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:one_clip_board/theme.dart';
 import 'package:one_clip_board/views/screens/clip_list_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: [
+    SystemUiOverlay.bottom,
+  ]);
   runApp(const MyApp());
 }
 
@@ -12,6 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Welcome to Flutter',
       theme: appThemeData(),
       home: const ClipListScreen(),
