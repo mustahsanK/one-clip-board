0210eb149b42a0d019c71ebb31f3c4d38b1e3df5
mustahsanK <mustahsan60@outlook.com>
2023-07-07 22:28:12 +0530

    updated Clip Details Screen

--- lib/views/widgets/clip_detatils.dart
+++ lib/views/widgets/clip_detatils.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:one_clip_board/model/clip.dart';
+import 'package:one_clip_board/views/widgets/cat_icon.dart';
 
 class ClipDetails extends StatelessWidget {
   final Clipping clip;
@@ -8,30 +9,35 @@ class ClipDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Row(
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(clip.uid.toString())
+            Row(
+              children: [
+                CategoryIcon(clip.collection.sId, size: 30),
+                const SizedBox(width: 8),
+                Text(clip.collection.name, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Card(
+              child: Text(clip.data, style: const TextStyle(fontSize: 24)),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(clip.date),
+                Text('source: ${clip.source}'),
+              ],
+            ),
           ],
         ),
-        Card(
-          child: Column(
-            children: [
-              Text(clip.data),
-              const SizedBox(height: 10),
-              Row(
-                children: [
-                  Expanded(child: Text(clip.source)),
-                  Expanded(child: Text(clip.date)),
-                ],
-              )
-            ],
-          ),
-        )
-      ],
+      ),
     );
   }
 }
